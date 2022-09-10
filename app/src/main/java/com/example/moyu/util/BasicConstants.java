9227927580bf1463dd1f47c5668fd98010c76494
fujiehhu <1163844624@qq.com>
2022-09-10 16:39:20 +0800

    摸鱼1.2

--- app/src/main/java/com/example/moyu/util/BasicConstants.java
+++ app/src/main/java/com/example/moyu/util/BasicConstants.java
@@ -3,7 +3,7 @@ package com.example.moyu.util;
 public enum BasicConstants {
 
     ZHOUMO("周末", "2022-09-10 00:00:00"),
-    ZHONGQIU("中秋节", "2022-09-10 00:00:00"),
+    ZHONGQIU("中秋节", "2023-09-29 00:00:00"),
     GUOQING("国庆节", "2022-10-01 00:00:00"),
     YUANDAN("元旦", "2023-01-01 00:00:00"),
     CHUNJIE("春节", "2023-02-11 00:00:00"),
